3f3dda4713b491a2504c1b2c38e2c807e70a7893
Yash Nimje <yashnimje2005@gmail.com>
2025-05-04 09:10:02 +0100

    Time: 0 ms (100%), Space: 42.1 MB (25.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,16 +5,17 @@ public:
         vector<int> res(n);
         vector<int> prepro(n);
         vector<int> sufpro(n);
-
+        //prefix product formation in new array
         prepro[0]=nums[0];
         for(int i = 1; i < n; i++ ){
             prepro[i] = prepro[i-1] * nums[i]; 
         }
-
+        //suffix product formation in new array
         sufpro[n-1] = nums[n-1];
         for(int i = n-2; i>=0 ; i--){
             sufpro[i] = sufpro[i+1] * nums[i];
         }
+        //taking both array, i+1 and i-1 number and taking product and putting it in resultant array
         int i = 0;
         int product =1;
         while(i<n){
