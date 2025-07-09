6aed0f16d329baf39d6fa066637c417391bb78a3
Yash Nimje <yashnimje2005@gmail.com>
2025-07-09 15:47:42 +0100

    Time: 36 ms (66.47%), Space: 18.6 MB (23.79%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -4,23 +4,61 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* a = headA;
         ListNode* b = headB;
-        ListNode* ans;
+        int lenA = 0;
+        int lenB = 0;
+
         bool say = false;
 
         while(a != NULL){
-            b = headB;
-            while(b != NULL){
-                if(b == a){
-                    ans = a;
-                    say = true;
-                    break;
-                }
-                b = b->next;
-            }
-            if(b==a) break;
+            lenA++;
             a = a->next;
-        }   
-        if(say == true) return ans;
+        }
+
+        while(b != NULL){
+            lenB++;
+            b = b->next;
+        }
+
+        a = headA;
+        b = headB;
+        
+        for(int i = 1; i <= abs(lenA-lenB);i++){
+            if(lenA>lenB) a = a->next;
+            else b = b->next;
+        }
+
+        while(a != NULL){
+            if(a==b){
+                say = true;
+                break;
+            }
+            b =b -> next;
+            a =a -> next;
+        }
+        if(say == true) return a;
         else return NULL;
+        
+        
+        
+        
+        
+        
+        
+        // bool say = false;
+
+        // while(a != NULL){
+        //     b = headB;
+        //     while(b != NULL){
+        //         if(b == a){
+        //             say = true;
+        //             break;
+        //         }
+        //         b = b->next;
+        //     }
+        //     if(b==a) break;
+        //     a = a->next;
+        // }   
+        // if(say == true) return a;
+        // else return NULL;
     }
 };
