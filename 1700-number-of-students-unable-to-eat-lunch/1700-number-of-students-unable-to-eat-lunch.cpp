1ef724bb24f69048054a5217e8414d0fd551b7e3
Yash Nimje <yashnimje2005@gmail.com>
2025-08-09 22:13:15 +0530

    Time: 0 ms (100%), Space: 11.8 MB (71.37%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -9,10 +9,8 @@ public:
         for(int i = 0;i < students.size();i++){
             q.push(students[i]);
         }
-
-        bool flag = true;
         int num_push = 0;
-        while(flag){
+        while(q.size()>0 && num_push != q.size()){
             if(st.size() == 0) break;
             if(st.top() == q.front()){
                 st.pop();
@@ -20,15 +18,35 @@ public:
                 num_push = 0;
             }
             else{ // st.top() != q.front()
-                if(num_push == q.size()){
-                    flag = false;
-                }
                 q.push(q.front());
                 q.pop();
                 num_push++;
             }
-
         }
         return q.size();
     }
 };
+
+// class Solution {
+// public:
+//     int countStudents(vector<int>& students, vector<int>& sandwiches) {
+//         int ones = 0, zeros = 0;
+        
+//         for (int s : students) {
+//             if (s == 1) ones++;
+//             else zeros++;
+//         }
+
+//         for (int s : sandwiches) {
+//             if (s == 1) {
+//                 if (ones == 0) return zeros;
+//                 ones--;
+//             } else {
+//                 if (zeros == 0) return ones;
+//                 zeros--;
+//             }
+//         }
+
+//         return 0; // All students are served
+//     }
+// };
