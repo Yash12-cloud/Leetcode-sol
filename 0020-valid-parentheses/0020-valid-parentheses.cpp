26a2b002f50b6251fc2ba3573cfe1b767ae2e51d
Yash Nimje <yashnimje2005@gmail.com>
2025-08-22 11:36:39 +0530

    Time: 0 ms (100%), Space: 8.8 MB (33.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length() % 2 != 0) return false;
+        stack<char> st1; // for ()
+        // stack<char> st2; // for {}
+        // stack<char> st3; // for []
+
+        for(int i = 0;i<s.size();i++){
+            if(s[i]=='(') st1.push(s[i]);
+            else if(s[i] == '{') st1.push(s[i]);
+            else if(s[i] == '[') st1.push(s[i]);
+
+            if(s[i]==')'){
+                if(st1.size()==0) return false;
+                else if(st1.top()=='('){
+                    st1.pop();
+                }
+                else return false;
+            }
+            else if(s[i]=='}'){
+                if(st1.size()==0) return false;
+               else if(st1.top()=='{') {
+                st1.pop();
+               }
+               else return false;
+            }
+            else if(s[i]==']'){
+                if(st1.size()==0) return false;
+                else if(st1.top()=='['){
+                    st1.pop();
+                }
+                else return false;
+            }
+        }
+        if(st1.size()==0) return true;
+        else return false;
+    }
+};
