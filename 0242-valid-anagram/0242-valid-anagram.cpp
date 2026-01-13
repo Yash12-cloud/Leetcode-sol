470038ccb81c2f8bbe0c3ca943a1c1a9af3e3311
Yash Nimje <yashnimje2005@gmail.com>
2026-01-13 20:43:35 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s == t) return true;
-        else return false;
+        if(s.size() != t.size()) return false;
+        // Using  hashmap
+        unordered_map<char,int> map1;
+        for(int i =0 ; i<s.size();i++){
+            map1[s[i]]++;
+            map1[t[i]]--;// saat saat ma freq subtract krta rahuga
+            //agar element exist nhi krta hoga tho vo create krke map ma daldega
+            // aur ya agar element exist krta hoga tho uski frequency 1 sa increment krdega
+        }
+
+        for(auto p : map1){
+            if(p.second != 0) return false;
+        }
+        return true;
+        
+
+
+
+        // Using sorting 
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s == t) return true;
+        // else return false;
     }
 };
