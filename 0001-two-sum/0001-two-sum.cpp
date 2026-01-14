d15f00ae0ce7dfd7af8acc5e09c73422c7bf0cb4
Yash Nimje <yashnimje2005@gmail.com>
2026-01-14 20:51:06 +0530

    Time: 0 ms (100%), Space: 14.7 MB (61.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,16 +3,29 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<int> ans;
-
-        for(int i =0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                int sum = nums[i]+nums[j];
-                if(sum==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+        // METHOD -2 : using Maps
+        unordered_map<int,int> m;
+        for(int i = 0 ;i < n; i++){
+            if(m.find(target - nums[i]) != m.end()){
+                ans.push_back(i);
+                ans.push_back(m[target-nums[i]]);
+                break;
+            }
+            else{
+                m[nums[i]] = i;
             }
         }
+
+        // METHOD -1 : Brute force
+        // for(int i =0;i<n-1;i++){
+        //     for(int j = i+1;j<n;j++){
+        //         int sum = nums[i]+nums[j];
+        //         if(sum==target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
         return ans;
     }
 };
