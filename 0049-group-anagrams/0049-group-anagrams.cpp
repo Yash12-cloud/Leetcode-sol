28f5ea217a2085fe00cb1ea0c446ac5da7788001
Yash Nimje <yashnimje2005@gmail.com>
2026-02-01 19:51:45 +0530

    Time: 8 ms (95.7%), Space: 26 MB (48.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& arr) {
         int n = arr.size();
         unordered_map<string,vector<string>> mp;
-        for(auto x : arr){
-            sort(x.begin(),x.end());
-            mp[x];
-        }
+        // for(auto x : arr){
+        //     sort(x.begin(),x.end());
+        //     mp[x];
+        // }
         for(auto x : arr){
             auto p = x;
             sort(x.begin(),x.end());
