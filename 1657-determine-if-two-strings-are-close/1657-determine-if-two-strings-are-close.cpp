b2cb4556acf65a71740bf5301e1f03f6bda76859
Yash Nimje <yashnimje2005@gmail.com>
2026-02-02 19:08:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if(word1.size() != word2.size()) return false;
+        int n = word1.size();
+        unordered_map<char,int> s1;
+        unordered_map<char,int> s2;
+        unordered_map<int,int> freq1;
+        unordered_map<int,int> freq2;
+        for(int i = 0 ;i < n; i++){
+            s1[word1[i]]++;
+            s2[word2[i]]++;
+        }
+
+        for(auto x : s1){
+            freq1[x.second]++;
+        }
+        for(auto x : s2){
+            freq2[x.second]++;
+        }
+        if(freq1 == freq2) return true;
+        else return false;
+    }
+};
