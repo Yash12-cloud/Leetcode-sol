0043024b450d207c15603a5292d02e11eee8d11e
Yash Nimje <yashnimje2005@gmail.com>
2026-03-21 20:00:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    typedef pair<int,int> pi;
+    typedef pair<int, int> pi;
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int,int> m;
-        priority_queue< pi,vector<pi>,greater<pi> > pq; // min heap
+        unordered_map<int, int> m;
+        priority_queue<pi, vector<pi>, greater<pi>> pq; // min heap
 
-        for(auto p : nums){
+        for (auto p : nums) {
             m[p]++;
         }
-        for(auto p : m){
-            //freq ko key banaya and key ko value
-            // heap phale pair ka 1st element ko compare krta hai agar vo same hua tab sencond ko compare krega
-            pq.push({p.second, -p.first});//-p.first isliya liya kyuki humko descend order ma cheya 
-            // 3>2 isliya 2 wala heap ma top ma rahega aur uski hi freq phale ans array ma jaygi usko rokne kaliya humne dono ko -ve krdiya ab -3<-2 tho -3 top ma rahega jab dono ki freq same hogi
+        for (auto p : m) {
+            // freq ko key banaya and key ko value
+            //  heap phale pair ka 1st element ko compare krta hai agar vo same
+            //  hua tab sencond ko compare krega
+            pq.push({p.second, -p.first}); //-p.first isliya liya kyuki humko
+                                           // descend order ma cheya
+            // 3>2 isliya 2 wala heap ma top ma rahega aur uski hi freq phale
+            // ans array ma jaygi usko rokne kaliya humne dono ko -ve krdiya ab
+            // -3<-2 tho -3 top ma rahega jab dono ki freq same hogi
         }
         vector<int> ans;
-        while(!pq.empty()){
+        while (!pq.empty()) {
             auto top = pq.top();
             pq.pop();
-            for(int i = 0; i < top.first; i++){ //loop chalaya taki ma uski freq utna loop chala saku pushback krne ko
+            for (int i = 0; i < top.first;
+                 i++) { // loop chalaya taki ma uski freq utna loop chala saku
+                        // pushback krne ko
                 ans.push_back(-top.second);
             }
         }
         return ans;
     }
 };
+// key concept :Custom sorting using a hash map for frequency counting and a priority queue with negated values for tie-breaking.
