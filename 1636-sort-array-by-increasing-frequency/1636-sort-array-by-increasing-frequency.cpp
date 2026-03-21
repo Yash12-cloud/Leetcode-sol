efd9988d719ca4a30fc2797ff2a9774350b12d20
Yash Nimje <yashnimje2005@gmail.com>
2026-03-21 19:55:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -9,7 +9,10 @@ public:
             m[p]++;
         }
         for(auto p : m){
-            pq.push({p.second, -p.first});//freq ko key banaya and key ko value
+            //freq ko key banaya and key ko value
+            // heap phale pair ka 1st element ko compare krta hai agar vo same hua tab sencond ko compare krega
+            pq.push({p.second, -p.first});//-p.first isliya liya kyuki humko descend order ma cheya 
+            // 3>2 isliya 2 wala heap ma top ma rahega aur uski hi freq phale ans array ma jaygi usko rokne kaliya humne dono ko -ve krdiya ab -3<-2 tho -3 top ma rahega jab dono ki freq same hogi
         }
         vector<int> ans;
         while(!pq.empty()){
