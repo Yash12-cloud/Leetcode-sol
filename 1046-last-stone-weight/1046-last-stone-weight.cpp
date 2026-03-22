9b9c9ea9aaaeab2569a13b47f1cebdeb620ac8b9
Yash Nimje <yashnimje2005@gmail.com>
2026-03-22 21:07:07 +0530

    Time: 3 ms (4.6%), Space: 10 MB (12.09%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        // without heap O(n2 log n)
-        while(stones.size() > 1){
-            sort(stones.begin(),stones.end());
-            int temp1 = stones[stones.size()-1];
-            stones.pop_back();
-            int temp2 = stones[stones.size()-1];
-            stones.pop_back();
-            int x = min(temp1,temp2) ;
-            int y = max(temp1,temp2);
-            
-            stones.push_back(y-x);
+        priority_queue<int> pq;
+        for(auto x : stones){
+            pq.push(x);
         }
-        return stones[0];
+        while(pq.size()>1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            pq.push(y-x);
+        }
+        int ans = pq.top();
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        // without heap O(n2 log n)
+        // while(stones.size() > 1){
+        //     sort(stones.begin(),stones.end());
+        //     int temp1 = stones[stones.size()-1];
+        //     stones.pop_back();
+        //     int temp2 = stones[stones.size()-1];
+        //     stones.pop_back();
+        //     int x = min(temp1,temp2); // hamesha do sabse bhari patthar phale lena hai
+        //     int y = max(temp1,temp2);
+            
+        //     stones.push_back(y-x);
+        // }
+        // return stones[0];
 
     }
 };
