87044eb0826fc058193238fbcae514b75a238d03
Yash Nimje <yashnimje2005@gmail.com>
2026-03-22 19:24:46 +0530

    Time: 0 ms (100%), Space: 10 MB (49.45%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while(stones.size() > 1){
+            sort(stones.begin(),stones.end());
+            int temp1 = stones[stones.size()-1];
+            stones.pop_back();
+            int temp2 = stones[stones.size()-1];
+            stones.pop_back();
+            int x = min(temp1,temp2) ;
+            int y = max(temp1,temp2);
+            
+            stones.push_back(y-x);
+        }
+        return stones[0];
+
+    }
+};
