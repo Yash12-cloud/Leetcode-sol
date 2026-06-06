5eba766f02b5960148d781ddf11d9ba6bc60c43b
Yash Nimje <yashnimje2005@gmail.com>
2026-06-06 21:58:38 +0530

    Time: 43 ms (77.6%), Space: 93.8 MB (73.23%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,26 @@
+bool cmp(vector<int> &a, vector<int> &b){
+    return a[1] < b[1];
+}
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int n = points.size();
+        // jitna non overlapping interval hoge utna hi arrow lagege
+        // so interval ki condition dheklo kya = consider hai ya nhi
+        sort(points.begin(),points.end(),cmp);
+
+        int lastendtime = points[0][1];
+
+        int ans = 0;
+
+        for(int i =  1; i < n; i++){
+            if(points[i][0] <= lastendtime){ // overlapping or not
+                ans++;
+            }
+            else{
+                lastendtime = points[i][1];
+            }
+        }
+        return points.size() - ans;// total interval - non overlapping interval count 
+    }
+};
